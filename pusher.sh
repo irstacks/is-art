44d3ccad4695428eb07debd709ed50919a7f8ecd
Mr. Is <isaac.ardis@gmail.com>
2015-12-03 13:33:18 -0500

    add bower comp to pusher

--- pusher.sh
+++ pusher.sh
@@ -3,3 +3,6 @@ cp ~/dev/isaacardis/.tmp/styles/main.css ~/dev/isaacardis/app/styles/main.css
 
 # Da whole dang ding.
 rsync -avz --update ~/dev/isaacardis/app/ areteh.co:isaacardis/
+
+# Bower components.
+rsync -avz --update ~/dev/isaacardis/bower_components/ areteh.co:isaacardis/bower_components/
